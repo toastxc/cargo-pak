1df9f192aff6e126c082b95c10d6a29e30bc5f09
ToastXC <100072983+toastxc@users.noreply.github.com>
2024-08-30 11:37:14 +0800

    added go!

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,42 @@ use crate::data::yaml::ManifestYaml;
 use crate::dep_check::check;
 use crate::flatpak::Flatpak;
 use crate::shell::Shell;
+use clap::{Arg, Command};
 use std::env::args;
 
 pub type Result<T> = core::result::Result<T, anyhow::Error>;
 
 fn main() {
     check();
-    let mut args: Vec<String> = args().collect();
-    args.remove(0);
+    //let mut args: Vec<String> = args().collect();
+    //args.remove(0);
 
-    println!("args: {:?}", args);
+    let cmd = Command::new("cargo-pak")
+        .version(clap::crate_version!())
+        .subcommand(Command::new("generate").alias("gen"))
+        .subcommand(Command::new("build"))
+        .subcommand(Command::new("install"))
+        .subcommand(Command::new("remove"))
+        .subcommand(Command::new("go"));
 
-    let Some(arg) = args.first() else { return };
+    let matcher = cmd.clone().get_matches();
+
+    let matcher = match matcher.subcommand() {
+        None => {
+            println!("`cargo-pak help` for help");
+            return;
+        }
+        Some((matcher, _)) => matcher,
+    };
 
     println!("Cargo_pack starting...");
     if let Err(error) = {
-        match arg.to_lowercase().as_str() {
+        match matcher {
             "generate" => ManifestYaml::generate().map(|_| ()),
             "build" => build().map(|_| ()),
             "install" => install(),
             "remove" => remove(),
+            "go" => go(),
             _ => Ok(println!("Invalid command")),
         }
     } {
@@ -34,6 +50,14 @@ fn main() {
     } else {
         println!("Done!");
     }
+}
+
+pub fn go() -> Result<()> {
+    remove()?;
+    ManifestYaml::generate()?;
+    build()?;
+    install()?;
+    Ok(())
 }
 
 fn remove() -> Result<()> {
